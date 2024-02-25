872b32846f5e0c5f9e5b7c231fa4bfa56fc9d66c
duoduo70 <1499874170@qq.com>
2024-02-25 16:38:33 +0800

    增加了默认的global_allocator

--- src/main.rs
+++ src/main.rs
@@ -2,10 +2,27 @@
 #![no_main]
 
 extern crate libc;
+extern crate nalloc;
 
 use core::panic::PanicInfo;
 
 use libc::printf;
+
+struct MyAllocator;
+
+unsafe impl core::alloc::GlobalAlloc for MyAllocator {
+    unsafe fn alloc(&self, layout: core::alloc::Layout) -> *mut u8 {
+        libc::malloc(layout.size()) as *mut u8
+    }
+
+    unsafe fn dealloc(&self, ptr: *mut u8, _layout: core::alloc::Layout) {
+        libc::free(ptr as *mut libc::c_void);
+    }
+}
+
+#[global_allocator]
+static GLOBAL: MyAllocator = MyAllocator;
+
 
 #[panic_handler]
 fn panic(_info: &PanicInfo) -> ! {
